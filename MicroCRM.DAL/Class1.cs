5962e44ffe171d02e79f9a7ebdd2f8828d933cff
rajesh <rajesh.dnp01@gmail.com>
2021-01-20 10:03:54 +0530

    Add project files.

---
+++ MicroCRM.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MicroCRM.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ MicroCRM.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MicroCRM.Models
+{
+    public class Class1
+    {
+    }
+}
